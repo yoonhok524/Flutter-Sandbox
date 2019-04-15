c03b9a0fc7450596f17aced1e40c169563ae34dd
KimYoonho Aaron <youknow.yoonho@gmail.com>
2019-04-16 01:09:35 +0900

    Alarm Manager

---
+++ flutter_alarm_manager/lib/main.dart
@@ -0,0 +1,58 @@
+import 'dart:isolate';
+import 'package:flutter/material.dart';
+import 'package:android_alarm_manager/android_alarm_manager.dart';
+
+void printHello() {
+  final DateTime now = DateTime.now();
+  final int isolateId = Isolate.current.hashCode;
+  print("[$now] Hello, world! isolate=$isolateId function='$printHello'");
+}
+
+main() async {
+  final int helloAlarmID = 0;
+  await AndroidAlarmManager.initialize();
+  runApp(MyApp());
+
+  // Periodic
+  await AndroidAlarmManager.periodic(
+      const Duration(minutes: 1), helloAlarmID, printHello);
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key key, this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: Text(
+          'You have pushed the button this many times:',
+        ),
+      ),
+    );
+  }
+}
