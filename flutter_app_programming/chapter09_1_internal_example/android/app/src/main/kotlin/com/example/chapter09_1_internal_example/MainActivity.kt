8c1979ddb66b617ada9bbc353293983626904c0a
Yoonho Aaron Kim <yoonhok524@users.noreply.github.com>
2022-11-21 09:54:14 +0900

    chapter09-1

---
+++ flutter_app_programming/chapter09_1_internal_example/android/app/src/main/kotlin/com/example/chapter09_1_internal_example/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.chapter09_1_internal_example
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ flutter_app_programming/chapter09_1_internal_example/lib/main.dart
@@ -0,0 +1,89 @@
+import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  int _counter = 0;
+  final _key = 'count';
+
+  @override
+  void initState() {
+    super.initState();
+    _loadData();
+  }
+
+  void _incrementCounter() {
+    setState(() {
+      _counter++;
+    });
+    _setData(_counter);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            const Text(
+              'You have pushed the button this many times:',
+            ),
+            Text(
+              '$_counter',
+              style: Theme.of(context).textTheme.headline4,
+            ),
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _incrementCounter,
+        tooltip: 'Increment',
+        child: const Icon(Icons.add),
+      ),
+    );
+  }
+
+  void _setData(int value) async {
+    SharedPreferences pref = await SharedPreferences.getInstance();
+    pref.setInt(_key, value);
+  }
+
+  void _loadData() async {
+    SharedPreferences pref = await SharedPreferences.getInstance();
+    setState(() {
+      final value = pref.getInt(_key);
+      _counter = (value != null) ? value : 0;
+    });
+  }
+}
